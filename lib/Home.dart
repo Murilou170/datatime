0b124eb4c26c6c6e909235219a3c45e35a938a30
murilo <murilra@hotmail.com>
2022-04-22 15:03:49 -0300

    pronto

--- lib/Home.dart
+++ lib/Home.dart
@@ -38,20 +38,19 @@ class _HomeState extends State<Home> {
         title: Text('Data'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-         child: Center(
-              child: ElevatedButton(onPressed: _showDatePicker,
-                  child:
-                  Text(
-                    _selectedDate == null
-                        ? 'nenhuma data selecionada!'
-                        : DateFormat('d/MMMM/y').format(_selectedDate!),
-                  )
-              )
-          )
-      )
+      body: Center(
+           child: ElevatedButton(
+               onPressed: _showDatePicker,
+               child:
+               Text(
+                 _selectedDate == null
+                     ? 'nenhuma data selecionada!'
+                     : DateFormat('d/MMMM/y').format(_selectedDate!),
+               )
+           )
+       )
     );
   }
 }
 // MediaQuery.of(context).textScaleFactor para acessibilidade e responsividade da fonte de acordo com o usuário
-// constraints.maxHeight * 0.5
+// constraints.maxHeight
